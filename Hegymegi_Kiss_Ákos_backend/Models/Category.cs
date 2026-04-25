9cf816d535ff920ff1bc0a8f8b408671c566b02f
Hegymegi-Akos <90569793+Hegymegi-Akos@users.noreply.github.com>
2026-04-25 15:58:49 +0200

    Update Category.cs

--- Hegymegi_Kiss_Ákos_backend/Models/Category.cs
+++ Hegymegi_Kiss_Ákos_backend/Models/Category.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Hegymegi_Kiss_Ákos_backend.Models;
@@ -8,4 +8,8 @@ public partial class Category
     public int CategoryId { get; set; }
 
     public string CategoryName { get; set; } = null!;
+
+    // A Books lista KELL a feladat 11-hez!
+    // Eredetileg hiányzott a generált modellből, ezért az Include() elszállt.
+    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
 }
